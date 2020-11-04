3524d9b8656a08ebef494aa011170541a0fcf38e
janghyoseog <bestdevhyo1225@gmail.com>
2020-11-04 21:17:47 +0900

    test: 테스트 코드 수정

--- admin-api/src/test/java/com/hyoseok/ProductAdminApiApplicationTest.java
+++ admin-api/src/test/java/com/hyoseok/ProductAdminApiApplicationTest.java
@@ -1,5 +1,15 @@
+package com.hyoseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ProductAdminApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
--- admin-api/src/test/java/com/hyoseok/api/AdminApiControllerTest.java
+++ admin-api/src/test/java/com/hyoseok/api/AdminApiControllerTest.java
@@ -1,29 +1,24 @@
 package com.hyoseok.api;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @WebMvcTest(AdminApiController.class)
 @DisplayName("Admin API 테스트")
 public class AdminApiControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    @DisplayName("상품을 생성한다.")
-    public void createProduct() throws Exception {
-
-    }
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    @DisplayName("상품을 생성한다.")
+//    public void createProduct() throws Exception {
+//
+//    }
 
 }
