aa5baafc31225e0eddc09a24f062ebd55fac0b20
janghyoseog <bestdevhyo1225@gmail.com>
2020-11-14 14:56:13 +0900

    chore: 'batch' 모듈 개발 환경 세팅

---
+++ batch/src/main/java/com/hyoseok/job/SimpleJobConfiguration.java
@@ -0,0 +1,39 @@
+package com.hyoseok.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> Step1 <<<<<");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+
+    }
+
+}
